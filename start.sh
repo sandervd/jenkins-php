0018b53e81cfebf3b564f1e931066ad2babe1557
Gregory Boddin <gboddin@users.noreply.github.com>
2015-09-07 10:04:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ then
 	exit 1
 fi
 #PID=`docker run -d -p 8080:8080 -u root -v /home/tinne/playground/jenkins:/var/jenkins_home jenkins`
-PID = `docker run -p 8080:8080 -u root jenkins`
+PID=`docker run -p 8080:8080 -u root jenkins`
 echo $PID > .docker-id
 echo "backgrounded"
 docker attach $PID
